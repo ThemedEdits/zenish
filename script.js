ff5d0e0f83596f20fd45574f94ba3408ce67cf2b
ThemedEdits <themed.edits.co@gmail.com>
2025-03-24 23:34:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset();
     });
 });
+
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
